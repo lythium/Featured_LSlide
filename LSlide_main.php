9896856078df99d9d4ebd30e567167c3d70e05c0
lythium <niko.phoenix1@gmail.com>
2017-07-22 16:20:30 +0200

    update repository

--- LSlide_main.php
+++ LSlide_main.php
@@ -15,14 +15,8 @@ class LSlide_Plugin
     {
         add_action('admin_menu', array($this, 'add_admin_menu'));
 
-        include_once plugin_dir_path(__FILE__).'class/LSlide_add.php';
-        new Add_LSlide();
-
         include_once plugin_dir_path(__FILE__).'class/LSlide_shortcode.php';
         new Shortcode_LSlide();
-
-        include_once plugin_dir_path(__FILE__).'class/LSlide_update.php';
-        new Update_LSlide();
 
         add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_styles' ));
         // add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_lSlide_scripts' ));
@@ -37,7 +31,6 @@ class LSlide_Plugin
 	public function add_admin_menu()
     {
         add_menu_page('Lyth Slide Plugin', 'LSlide plugin', 'manage_options', 'LSlide', array($this, 'menu_html'));
-        add_submenu_page('LSlide', 'Apercu des Slides', 'Apercu des Slides', 'manage_options', 'LSlide', array($this, 'menu_html'));
     }
 
     public function menu_html()
