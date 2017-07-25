06e0a22c111bfaf7420e59a56fee709cf34cef48
lythium <niko.phoenix1@gmail.com>
2017-07-25 17:01:30 +0200

    intend fix

--- LSlide_main.php
+++ LSlide_main.php
@@ -11,47 +11,47 @@ License:
 
 class LSlide_Plugin
 {
-    public function __construct()
-    {
-        add_action('admin_menu', array($this, 'add_admin_menu'));
+	public function __construct()
+	{
+		add_action('admin_menu', array($this, 'add_admin_menu'));
 
-        include_once plugin_dir_path(__FILE__).'class/LSlide_init.php';
-        new Initialise_LSlide();
+		include_once plugin_dir_path(__FILE__).'class/LSlide_init.php';
+		new Initialise_LSlide();
 
-        include_once plugin_dir_path(__FILE__).'class/LSlide_shortcode.php';
-        new Shortcode_LSlide();
+		include_once plugin_dir_path(__FILE__).'class/LSlide_shortcode.php';
+		new Shortcode_LSlide();
 
-        add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_styles' ));
-        add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_scripts' ));
+		add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_styles' ));
+		add_action('wp_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_scripts' ));
 
 		add_action('admin_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_styles_admin' ));
 		add_action('admin_enqueue_scripts', array( __CLASS__, 'enqueue_LSlide_scripts_admin' ));
 
-        register_activation_hook(__FILE__, array('Initialise_LSlide', 'install'));
-        register_uninstall_hook(__FILE__, array('Initialise_LSlide', 'uninstall'));
-    }
+		register_activation_hook(__FILE__, array('Initialise_LSlide', 'install'));
+		register_uninstall_hook(__FILE__, array('Initialise_LSlide', 'uninstall'));
+	}
 
 	public function add_admin_menu()
-    {
-        add_menu_page('Lyth Slide Plugin', 'LSlide plugin', 'manage_options', 'LSlide', array($this, 'menu_html'));
-    }
+	{
+		add_menu_page('Lyth Slide Plugin', 'LSlide plugin', 'manage_options', 'LSlide', array($this, 'menu_html'));
+	}
 
-    public function menu_html()
-    {
-        include_once plugin_dir_path(__FILE__).'view/view_back_resume.php';
-    }
+	public function menu_html()
+	{
+		include_once plugin_dir_path(__FILE__).'view/view_back_resume.php';
+	}
 
-    public static function enqueue_LSlide_styles()
-    {
-        $css_file = plugins_url('css/LSlide_style_front.min.css', __FILE__);
-        wp_enqueue_style('LSlide_style_front', $css_file, false, "0.1");
-    }
+	public static function enqueue_LSlide_styles()
+	{
+		$css_file = plugins_url('css/LSlide_style_front.min.css', __FILE__);
+		wp_enqueue_style('LSlide_style_front', $css_file, false, "0.1");
+	}
 
-    public static function enqueue_LSlide_scripts()
-    {
-        $js_file = plugins_url('js/LSlide_slider_script.min.js', __FILE__);
-        wp_enqueue_script('LSlide_script', $js_file, array('jquery'), false, "0.1");
-    }
+	public static function enqueue_LSlide_scripts()
+	{
+		$js_file = plugins_url('js/LSlide_slider_script.min.js', __FILE__);
+		wp_enqueue_script('LSlide_script', $js_file, array('jquery'), false, "0.1");
+	}
 
 	public static function enqueue_LSlide_styles_admin()
 	{
