9bb9a06a3d3bb892ca8bda470cd81d1c5750e3c3
lythium <niko.phoenix1@gmail.com>
2017-08-01 08:02:29 +0200

    update front display

--- class/LSlide_shortcode.php
+++ class/LSlide_shortcode.php
@@ -3,12 +3,27 @@ class Shortcode_LSlide
 {
 	public function __construct()
 	{
-		add_shortcode('category_show', array($this, 'show_html'));
+		add_shortcode('LSlide', array($this, 'show_html'));
 	}
 
 	public function show_html($atts)
 	{
+        global $wpdb;
+        $default = $wpdb->get_results("SELECT LSlide_id FROM {$wpdb->prefix}LSlide_Config ORDER BY LSlide_id LIMIT 1");
 
+        $atts = shortcode_atts(array('id' => $default), $atts);
+        $id_shortcode = $atts["id"];
+        $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}LSlide_Config WHERE LSlide_id = '$id_shortcode'");
+
+        if ($results) {
+            foreach ($results as $key) {
+                $name_LSlide = $key->LSlide_Name;
+                $settings_LSlide = $key->LSlide_Settings;
+                ob_start();
+                include_once plugin_dir_path(__FILE__).'../view/view_front_slide.php';
+                return ob_get_clean();
+            }
+        }
 	}
 
 	public function substrwords($text, $maxchar, $end='...')
--- view/view_front_slide.php
+++ view/view_front_slide.php
@@ -1,33 +1,44 @@
 <?php
-$Image = "images/default.jpg";
-$Article = array('Article1', 'Article2', 'Article3', 'Article4', 'Article5', 'Article6');
+$args = array( 	'numberposts' => $settings_LSlide,
+'orderby' => 'post_date',
+'post_status' => 'publish',
+'order' => 'ASC' );
+$recent_posts = wp_get_recent_posts( $args );
 $count = 0;
 
  ?>
 <div class="row sliderApo">
 	<div class="container-slideApo">
 		<ul class="list-slideApo">
-		<?php foreach ( $Article as $post ): ?>
+		<?php foreach ( $recent_posts as $post ): ?>
+            <?php $imagePost = get_the_post_thumbnail($post["ID"], array(150, 150), array( 'class' => 'col-md-12 col-centered' ) ); ?>
+            <?php if (empty($imagePost)):
+                $imagePost = '/default.jpg';
+            endif; ?>
+            <?php $titlePost = $post["post_title"]; ?>
+            <?php $hrefPost = get_permalink($post["ID"]); ?>
 			<?php $first = ''; ?>
-			<?php if ($post === reset($Article)): ?>
+			<?php if ($post === reset($recent_posts)): ?>
 				<?php $first = "current"; ?>
 			<?php endif; ?>
 			<?php if ($count === 0):?>
 				<li class="list-item-sliderApo <?php echo 'list'.$count.' '.$first?>">
 			<?php endif; ?>
 					<div class="content-item">
-						<h5><?= $post ?></h5>
+						<h3>
+                            <a href="<?= $hrefPost ?>"> <?= $titlePost ?> </a>
+                        </h3>
 						<div class="item-list item-image1 animated flipInX">
-							<img src="<?= $Image ?>" alt="">
+							<img src="<?= $imagePost ?>" >
 						</div>
 						<div class="item-list item-image2 animated flipInY">
-							<img src="<?= $Image ?>" alt="">
+							<img src="<?= $imagePost ?>" >
 						</div>
 						<div class="item-list item-image3 animated flipInX">
-							<img src="<?= $Image ?>" alt="">
+							<img src="<?= $imagePost ?>" >
 						</div>
 					</div>
-			<?php if ($count === 2 || $post === end($Article)):?>
+			<?php if ($count === 2 || $post === end($recent_posts)):?>
 				</li>
 				<?php $count = 0; ?>
 			<?php else: ?>
