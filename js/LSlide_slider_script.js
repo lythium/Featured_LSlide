09b47da250db76d6d6b54fa4b941aadbb2860691
lythium <niko.phoenix1@gmail.com>
2017-08-06 10:37:52 +0200

    update script

--- js/LSlide_slider_script.js
+++ js/LSlide_slider_script.js
@@ -4,25 +4,33 @@ $(document).ready(function(){
 
     var currentIndex = 0,
     $Lists = $('.list-item-sliderApo'),
+    $item1 = $Lists.find(".item-image1"),
+    $item2 = $Lists.find(".item-image2"),
+    $item3 = $Lists.find(".item-image3"),
     $itemAmt = $Lists.length;
 
     $Click.click(function() {
         switchInterv($itemAmt);
     });
-    setInterval(function() {
-        switchInterv($itemAmt);
-    }, 7000);
+    // setInterval(function() {
+    //     switchInterv($itemAmt);
+    // }, 7000);
 
     function Anim(currentIndex) {
         var $List = $('.list-item-sliderApo').eq(currentIndex);
         AnimOut($Lists);
         AnimIn($List);
     };
+    function goQueue() {
+        $List.queue(function(){
+        });
+    };
+
     function AnimOut($Lists) {
         $Lists.find("h2").fadeOut(500);
-        $Lists.find(".item-image1").removeClass("flipInX").delay(500).addClass("flipOutX").fadeOut(1000);
-        $Lists.find(".item-image2").removeClass("flipInY").delay(500).addClass("flipOutY").fadeOut(1000);
-        $Lists.find(".item-image3").removeClass("flipInX").delay(500).addClass("flipOutX").fadeOut(1000);
+        $Lists.find(".item-image1").toggleClass("flipInX").toggleClass("flipOutX").fadeOut(1000);
+        $Lists.find(".item-image2").toggleClass("flipInY").toggleClass("flipOutY").fadeOut(1000);
+        $Lists.find(".item-image3").toggleClass("flipInX").toggleClass("flipOutX").fadeOut(1000);
         setTimeout(function(){
             $Lists.css("display", "none").removeClass("current");
         }, 1500)
@@ -30,9 +38,9 @@ $(document).ready(function(){
     function AnimIn($List) {
         setTimeout(function(){
             $List.css("display", "flex").addClass("current");
-            $List.find(".item-image1").removeClass("flipOutX").delay(500).addClass("flipInX").fadeIn(0);
-            $List.find(".item-image2").removeClass("flipOutY").delay(500).addClass("flipInY").fadeIn(0);
-            $List.find(".item-image3").removeClass("flipOutX").delay(500).addClass("flipInX").fadeIn(0);
+            $List.find(".item-image1").toggleClass("flipOutX").toggleClass("flipInX").fadeOut(1000);
+            $List.find(".item-image2").toggleClass("flipOutY").toggleClass("flipInY").fadeOut(1000);
+            $List.find(".item-image3").toggleClass("flipOutX").toggleClass("flipInX").fadeOut(1000);
             setTimeout(function(){
                 $List.find("h2").fadeIn(500);
             }, 500)
