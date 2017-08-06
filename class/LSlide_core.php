b476fb563cdc5f721a1d650a32e68d965873c92e
lythium <niko.phoenix1@gmail.com>
2017-08-06 10:37:18 +0200

    add class core

---
+++ class/LSlide_core.php
@@ -0,0 +1,30 @@
+<?php
+class Core_LSlide
+{
+    function __construct()
+    {
+        
+    }
+    public static function substrwords($text, $maxchar, $end='...')
+    {
+        // Cut string function
+        if (strlen($text) > $maxchar || $text == '') {
+            $words = preg_split('/\s/', $text);
+            $output = '';
+            $i      = 0;
+            while (1) {
+                $length = strlen($output)+strlen($words[$i]);
+                if ($length > $maxchar) {
+                    break;
+                } else {
+                    $output .= " " . $words[$i];
+                    ++$i;
+                }
+            }
+            $output .= $end;
+        } else {
+            $output = $text;
+        }
+        return $output;
+    }
+}
