62ab466e0b9c2cf51a02e4c455880a5060a1cece
lythium <niko.phoenix1@gmail.com>
2017-08-14 12:51:43 +0200

    update script admin

--- js/LSlide_admin_script.js
+++ js/LSlide_admin_script.js
@@ -22,18 +22,20 @@ $(document).ready(function(){
 
     $buttonUpdate.on('click',function(e){
         e.preventDefault();
-        var $id = this.id.substr(4);
-        var $hideResume = ".LSlide-display-backoff .hide-"+$id;
-        var $showResume = ".LSlide-display-backoff .show-"+$id;
+        var $id = this.id.substr(11);
+        var $hideResume = ".LSlide-display-backoff .hide-update_"+$id;
+        var $showResume = ".LSlide-display-backoff .show-update_"+$id;
         $($hideResume).hide(0);
         $($showResume).show(0);
+        $("form.update_LSlide_"+$id).parent().addClass("is-expanded");
     });
     $buttonCancelUpdate.on('click',function(e){
         e.preventDefault();
-        var $idCancel = this.id.substr(11);
-        var $hideResume = ".LSlide-display-backoff .hide-"+$idCancel;
-        var $showResume = ".LSlide-display-backoff .show-"+$idCancel;
+        var $idCancel = this.id.substr(18);
+        var $hideResume = ".LSlide-display-backoff .hide-update_"+$idCancel;
+        var $showResume = ".LSlide-display-backoff .show-update_"+$idCancel;
         $($hideResume).show(0);
         $($showResume).hide(0);
+        $("form.update_LSlide_"+$idCancel).parent().removeClass("is-expanded");
     });
 });
